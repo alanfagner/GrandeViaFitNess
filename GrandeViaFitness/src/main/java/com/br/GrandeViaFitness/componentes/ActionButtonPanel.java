af2224a1ff7befda0cbd510c91ab2a10f33c39e3
Alan Fagner Gonçalves <alan.f.goncalves@hotmail.com>
2014-08-11 18:07:50 -0300

    Adicionando gridGenerica s

---
+++ GrandeViaFitness/src/main/java/com/br/GrandeViaFitness/componentes/ActionButtonPanel.java
@@ -0,0 +1,50 @@
+package com.br.GrandeViaFitness.componentes;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.ajax.markup.html.AjaxLink;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.repeater.RepeatingView;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.PropertyModel;
+
+public class ActionButtonPanel<T> extends Panel
+{
+   private static final long serialVersionUID = 1324759387448672575L;
+   private final T entidade;
+   List<AjaxLink<T>> listBotoes = new ArrayList<AjaxLink<T>>();
+
+   public ActionButtonPanel(final String id, final IModel<T> model,
+      final List<AjaxLink<T>> listBotoes)
+   {
+      super(id);
+      this.entidade = model.getObject();
+      RepeatingView view = new RepeatingView("LinkList");
+      view.setOutputMarkupPlaceholderTag(true);
+      this.add(view);
+      this.listBotoes = listBotoes;
+
+      for (final AjaxLink<T> link : this.listBotoes)
+      {
+         WebMarkupContainer list = new WebMarkupContainer(view.newChildId());
+         AjaxLink<T> externalLink = new AjaxLink<T>("Link", new PropertyModel<T>(this, "entidade"))
+         {
+            private static final long serialVersionUID = 249147107071691478L;
+
+            @Override
+            public void onClick(final AjaxRequestTarget target)
+            {
+               link.setModel(this.getModel());
+               link.onClick(target);
+            }
+
+         };
+         externalLink.add(new Label("label", link.getId()));
+         list.add(externalLink);
+         view.add(list);
+      }
+   }
+}
