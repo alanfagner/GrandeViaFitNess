f0eee864dc880e20bbbfe71f611abde51ce882cf
alan.goncalves <alan.f.goncalves@hotmail.com>
2014-08-12 08:25:56 -0300

    Arrumando Conflitos

--- GrandeViaFitness/src/main/java/com/br/GrandeViaFitness/pages/visao/HomePage.java
+++ GrandeViaFitness/src/main/java/com/br/GrandeViaFitness/pages/visao/HomePage.java
@@ -2,7 +2,6 @@ package com.br.GrandeViaFitness.pages.visao;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.authroles.authorization.strategies.role.annotations.AuthorizeInstantiation;
@@ -15,7 +14,6 @@ import org.apache.wicket.markup.repeater.Item;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
-
 import com.br.GrandeViaFitness.componentes.ActionButtonPanel;
 import com.br.GrandeViaFitness.componentes.IDataSorteProvider;
 import com.br.GrandeViaFitness.model.Endereco;
@@ -25,125 +23,107 @@ import com.br.GrandeViaFitness.pages.visao.basePage.BasePage;
 public class HomePage extends BasePage
 {
 
-    private static final long serialVersionUID = -2280598124596767977L;
-    private IDataSorteProvider<Endereco, String> dataProvider;
-    private final List<AjaxLink<Endereco>> listBotoes = new ArrayList<AjaxLink<Endereco>>();
-    private DefaultDataTable<Endereco, String> gridGenerica;
+   private static final long serialVersionUID = -2280598124596767977L;
+   private IDataSorteProvider<Endereco, String> dataProvider;
+   private final List<AjaxLink<Endereco>> listBotoes = new ArrayList<AjaxLink<Endereco>>();
+   private DefaultDataTable<Endereco, String> gridGenerica;
 
-    public HomePage(final PageParameters parameters)
-    {
-        final List<IColumn<Endereco, String>> columns = new ArrayList<IColumn<Endereco, String>>();
+   public HomePage(final PageParameters parameters)
+   {
+      final List<IColumn<Endereco, String>> columns = new ArrayList<IColumn<Endereco, String>>();
 
-        listBotoes.add(new AjaxLink<Endereco>("Excluir")
-        {
-            private static final long serialVersionUID = -2007593370707695822L;
+      listBotoes.add(new AjaxLink<Endereco>("Excluir")
+      {
+         private static final long serialVersionUID = -2007593370707695822L;
 
-            @Override
-            public void onClick(final AjaxRequestTarget target)
-            {
-                dataProvider.getListaARetornar().remove(getModelObject());
-                dataProvider.size();
-                target.add(gridGenerica);
-            }
-<<<<<<< HEAD
-        });
+         @Override
+         public void onClick(final AjaxRequestTarget target)
+         {
+            dataProvider.getListaARetornar().remove(getModelObject());
+            dataProvider.size();
+            target.add(gridGenerica);
+         }
+      });
 
-        listBotoes.add(new AjaxLink<Endereco>("Visualizar")
-        {
-            private static final long serialVersionUID = -2007593370707695822L;
+      listBotoes.add(new AjaxLink<Endereco>("Visualizar")
+      {
+         private static final long serialVersionUID = -2007593370707695822L;
 
-            @Override
-            public void onClick(final AjaxRequestTarget target)
-            {
-                getModelObject();
-            }
-        });
+         @Override
+         public void onClick(final AjaxRequestTarget target)
+         {
+            getModelObject();
+         }
+      });
 
-=======
-        });
+      columns.add(new AbstractColumn<Endereco, String>(new Model<String>("Opções"))
+      {
+         private static final long serialVersionUID = -3102670641136395641L;
 
-        listBotoes.add(new AjaxLink<Endereco>("Visualizar")
-        {
-            private static final long serialVersionUID = -2007593370707695822L;
+         @Override
+         public void populateItem(final Item<ICellPopulator<Endereco>> cellItem,
+            final String componentId, final IModel<Endereco> entidade)
+         {
 
-            @Override
-            public void onClick(final AjaxRequestTarget target)
-            {
-                getModelObject();
-            }
-        });
+            cellItem.add(new ActionButtonPanel<Endereco>(componentId, entidade, listBotoes));
+         }
+      });
 
->>>>>>> origin/master
-        columns.add(new AbstractColumn<Endereco, String>(new Model<String>("Opções"))
-        {
-            private static final long serialVersionUID = -3102670641136395641L;
+      columns.add(new PropertyColumn<Endereco, String>(new Model<String>("Codigo"), "codigo")
+      {
 
-            @Override
-            public void populateItem(final Item<ICellPopulator<Endereco>> cellItem, final String componentId,
-                    final IModel<Endereco> entidade)
-            {
+         private static final long serialVersionUID = 3580594711515520158L;
 
-                cellItem.add(new ActionButtonPanel<Endereco>(componentId, entidade, listBotoes));
-            }
-        });
+         @Override
+         public String getCssClass()
+         {
+            return "numeric";
+         }
+      });
 
-        columns.add(new PropertyColumn<Endereco, String>(new Model<String>("Codigo"), "codigo")
-        {
+      columns.add(new PropertyColumn<Endereco, String>(new Model<String>("Logradouro"),
+         "logradouro", "logradouro"));
 
-            private static final long serialVersionUID = 3580594711515520158L;
-
-            @Override
-            public String getCssClass()
-            {
-                return "numeric";
-            }
-        });
-
-        columns.add(new PropertyColumn<Endereco, String>(new Model<String>("Logradouro"), "logradouro", "logradouro"));
-
-        columns.add(new PropertyColumn<Endereco, String>(new Model<String>("Bairro"), "bairro", "bairro")
-        {
-            /**
+      columns.add(new PropertyColumn<Endereco, String>(new Model<String>("Bairro"), "bairro",
+         "bairro")
+      {
+         /**
           *
           */
-            private static final long serialVersionUID = 3774551391770421047L;
+         private static final long serialVersionUID = 3774551391770421047L;
 
-            @Override
-            public String getCssClass()
-            {
-                return "last-name";
-            }
-        });
+         @Override
+         public String getCssClass()
+         {
+            return "last-name";
+         }
+      });
 
-        columns.add(new PropertyColumn<Endereco, String>(new Model<String>("Cidade"), "cidade"));
-        columns.add(new PropertyColumn<Endereco, String>(new Model<String>("Cep"), "cep"));
-<<<<<<< HEAD
+      columns.add(new PropertyColumn<Endereco, String>(new Model<String>("Cidade"), "cidade"));
+      columns.add(new PropertyColumn<Endereco, String>(new Model<String>("Cep"), "cep"));
+      gridGenerica = new DefaultDataTable<Endereco, String>("table", columns, getDataProvider(), 5);
+      gridGenerica.setOutputMarkupId(true);
+      this.add(gridGenerica);
+   }
 
-=======
->>>>>>> origin/master
-        gridGenerica = new DefaultDataTable<Endereco, String>("table", columns, getDataProvider(), 5);
-        gridGenerica.setOutputMarkupId(true);
-        this.add(gridGenerica);
-    }
-
-    public IDataSorteProvider<Endereco, String> getDataProvider()
-    {
-        if (dataProvider == null)
-        {
-            final List<Endereco> listaAPassar = new ArrayList<Endereco>();
-            for (int i = 0; i < 20; i++)
-            {
-                final Endereco ende = new Endereco();
-                ende.setLogradouro("Alamenda" + i);
-                ende.setEstado("SP");
-                ende.setCodigo(i);
-                ende.setCidade("Araraquara");
-                ende.setCep(14811060);
-                ende.setBairro("Vila Xavier" + i);
-                listaAPassar.add(ende);
-            }
-            dataProvider = new IDataSorteProvider<Endereco, String>(listaAPassar);
-        }
-        return dataProvider;
-    }
+   public IDataSorteProvider<Endereco, String> getDataProvider()
+   {
+      if (dataProvider == null)
+      {
+         final List<Endereco> listaAPassar = new ArrayList<Endereco>();
+         for (int i = 0; i < 20; i++)
+         {
+            final Endereco ende = new Endereco();
+            ende.setLogradouro("Alamenda" + i);
+            ende.setEstado("SP");
+            ende.setCodigo(i);
+            ende.setCidade("Araraquara");
+            ende.setCep(14811060);
+            ende.setBairro("Vila Xavier" + i);
+            listaAPassar.add(ende);
+         }
+         dataProvider = new IDataSorteProvider<Endereco, String>(listaAPassar);
+      }
+      return dataProvider;
+   }
 }
